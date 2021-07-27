476d2b92095f127292a46a99c5c2daaa5669f057
Kevin <kevinsevinche@gmail.com>
2021-07-27 14:49:38 -0500

    Add add-form event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+const ul = document.querySelector('ul');
+const form = document.querySelector('#add-form');
+
+const addScore = (e) => {
+  e.preventDefault();
+  const input = document.querySelectorAll('input');
+  const li = document.createElement('li');
+  const p = document.createElement('p');
+  p.innerHTML = `${input[0].value}: ${input[1].value}`;
+  li.appendChild(p);
+  ul.appendChild(li);
+  input[0].value = '';
+  input[1].value = '';
+};
+
+form.addEventListener('submit', addScore);
