c92f91dabf17cc7413e78bff9a92553b4d91f7a3
Kevin <kevinsevinche@gmail.com>
2021-07-28 12:27:55 -0500

    Add Async function to submit data to API

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import './style.css';
+// import './style.css';
 
-const ul = document.querySelector('ul');
-const form = document.querySelector('#add-form');
+const ul = document.querySelector("ul");
+const form = document.querySelector("#add-form");
 
-const addScore = (e) => {
+async function addScore(e) {
   e.preventDefault();
-  const input = document.querySelectorAll('input');
-  const li = document.createElement('li');
-  const p = document.createElement('p');
-  p.innerHTML = `${input[0].value}: ${input[1].value}`;
-  li.appendChild(p);
-  ul.appendChild(li);
-  input[0].value = '';
-  input[1].value = '';
-};
+  const input = document.querySelectorAll("input");
+  const response = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        user: input[0].value,
+        score: Number(input[1].value),
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 
-form.addEventListener('submit', addScore);
+  // const li = document.createElement("li");
+  // const p = document.createElement("p");
+  // p.innerHTML = `${input[0].value}: ${input[1].value}`;
+  // li.appendChild(p);
+  // ul.appendChild(li);
+  // input[0].value = "";
+  // input[1].value = "";
+}
+
+form.addEventListener("submit", addScore);
