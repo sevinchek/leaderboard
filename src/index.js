798336d1670f5c8a60cb13bf6e414353ac2991bb
Kevin <kevinsevinche@gmail.com>
2021-07-28 13:39:33 -0500

    Update functions to arrow functions

--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
-import './style.css';
+import "./style.css";
 
-const ul = document.querySelector('ul');
-const form = document.querySelector('#add-form');
-const refreshBtn = document.querySelector('#refresh');
+const ul = document.querySelector("ul");
+const form = document.querySelector("#add-form");
+const refreshBtn = document.querySelector("#refresh");
 
-async function addScore(e) {
+const addScore = async (e) => {
   e.preventDefault();
-  const input = document.querySelectorAll('input');
+  const input = document.querySelectorAll("input");
   const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores',
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores",
     {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         user: input[0].value,
         score: Number(input[1].value),
       }),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
-    },
+    }
   );
 
-  input[0].value = '';
-  input[1].value = '';
-}
+  input[0].value = "";
+  input[1].value = "";
+};
 
-async function refreshScoreTable() {
+const refreshScoreTable = async () => {
   const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores',
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores"
   )
     .then((response) => response.json())
     .then((json) => {
-      ul.innerHTML = '';
+      ul.innerHTML = "";
       json.result.forEach((element) => {
-        const li = document.createElement('li');
-        const p = document.createElement('p');
+        const li = document.createElement("li");
+        const p = document.createElement("p");
         p.innerHTML = `${element.user}: ${element.score}`;
         li.appendChild(p);
         ul.appendChild(li);
       });
     });
-}
+};
 
-form.addEventListener('submit', addScore);
-refreshBtn.addEventListener('click', refreshScoreTable);
-document.addEventListener('DOMContentLoaded', refreshScoreTable);
+form.addEventListener("submit", addScore);
+refreshBtn.addEventListener("click", refreshScoreTable);
+document.addEventListener("DOMContentLoaded", refreshScoreTable);
