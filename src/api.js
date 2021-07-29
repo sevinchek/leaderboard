1d996a01fcbb269254694785e63f001259a8a594
Kevin <kevinsevinche@gmail.com>
2021-07-29 10:00:17 -0500

    Fix Linter Errors

--- src/api.js
+++ src/api.js
@@ -1,35 +1,35 @@
-const ul = document.querySelector("ul");
+const ul = document.querySelector('ul');
 
 export const addScoreAPI = async (e) => {
   e.preventDefault();
-  const input = document.querySelectorAll("input");
+  const input = document.querySelectorAll('input');
   const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores",
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores',
     {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         user: input[0].value,
         score: input[1].value,
       }),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
-    }
+    },
   );
-  input[0].value = "";
-  input[1].value = "";
+  input[0].value = '';
+  input[1].value = '';
 };
 
 export const refreshScoreTableAPI = async () => {
   const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores"
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/clgFLMtHrYVTrf5UzXrW/scores',
   )
     .then((response) => response.json())
     .then((json) => {
-      ul.innerHTML = "";
+      ul.innerHTML = '';
       json.result.forEach((element) => {
-        const li = document.createElement("li");
-        const p = document.createElement("p");
+        const li = document.createElement('li');
+        const p = document.createElement('p');
         p.innerHTML = `${element.user}: ${element.score}`;
         li.appendChild(p);
         ul.appendChild(li);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import "./style.css";
-import { addScoreAPI, refreshScoreTableAPI } from "./api.js";
+import './style.css';
+import { addScoreAPI, refreshScoreTableAPI } from './api.js';
 
-const form = document.querySelector("#add-form");
-const refreshBtn = document.querySelector("#refresh");
+const form = document.querySelector('#add-form');
+const refreshBtn = document.querySelector('#refresh');
 
-form.addEventListener("submit", addScoreAPI);
-refreshBtn.addEventListener("click", refreshScoreTableAPI);
-document.addEventListener("DOMContentLoaded", refreshScoreTableAPI);
+form.addEventListener('submit', addScoreAPI);
+refreshBtn.addEventListener('click', refreshScoreTableAPI);
+document.addEventListener('DOMContentLoaded', refreshScoreTableAPI);
